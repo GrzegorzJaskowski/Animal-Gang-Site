a0d6323319db855a27cc335beeaec3afc50448d0
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-02 02:25:12 +0200

    feat: scroll to page item

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -3,6 +3,12 @@ import styles from "../styles/sidebarItem.module.css"
 function scrollToItem() {}
 
 export const SidebarItem = (props: { text: string }) => {
+  function handleClick() {
+    document
+      .getElementById(props.text.toLowerCase())
+      ?.scrollIntoView({ behavior: "smooth" })
+  }
+
   var id = "#"
   id += props.text.toLowerCase()
   return (
@@ -10,7 +16,7 @@ export const SidebarItem = (props: { text: string }) => {
     //   <p className={styles.header}>{props.text}</p>
     // </a>
 
-    <div className={styles.sidebarItem}>
+    <div onClick={handleClick} className={styles.sidebarItem}>
       <p className={styles.header}>{props.text}</p>
     </div>
   )

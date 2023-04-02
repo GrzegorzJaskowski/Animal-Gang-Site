02d1770d87bae1a0c140c987024e4e17ae64b440
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-02 20:55:56 +0200

    feat: change active on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,45 @@
 import styles from "../styles/sidebar.module.css"
 import { SidebarItem } from "../components/SidebarItem"
+import { useEffect, useState } from "react"
 
 export const Sidebar = () => {
-  const active = "Polar Bear"
+  const [active, setActive] = useState()
+  function changeActive(set: undefined) {
+    setActive(set)
+    console.log(set)
+  }
 
   return (
     <div className={styles.sidebar}>
       <p className={styles.header}>Your new gang</p>
       <div className={styles.itemBox}>
-        <SidebarItem text="Polar Bear" active={active} />
-        <SidebarItem text="Cheetah" active={active} />
-        <SidebarItem text="Panda" active={active} />
-        <SidebarItem text="Fox" active={active} />
-        <SidebarItem text="Squirrel" active={active} />
-        <SidebarItem text="Butterfly" active={active} />
-        <SidebarItem text="Elephant" active={active} />
+        <SidebarItem
+          text="Polar Bear"
+          active={active}
+          changeActive={changeActive}
+        />
+        <SidebarItem
+          text="Cheetah"
+          active={active}
+          changeActive={changeActive}
+        />
+        <SidebarItem text="Panda" active={active} changeActive={changeActive} />
+        <SidebarItem text="Fox" active={active} changeActive={changeActive} />
+        <SidebarItem
+          text="Squirrel"
+          active={active}
+          changeActive={changeActive}
+        />
+        <SidebarItem
+          text="Butterfly"
+          active={active}
+          changeActive={changeActive}
+        />
+        <SidebarItem
+          text="Elephant"
+          active={active}
+          changeActive={changeActive}
+        />
       </div>
     </div>
   )
--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react"
 import styles from "../styles/sidebarItem.module.css"
 
-export const SidebarItem = (props: { text: string; active: string }) => {
+export const SidebarItem = (props: {
+  text: string
+  active: undefined
+  changeActive: (params: any) => any
+}) => {
   function handleClick() {
     document
       .getElementById(props.text.toLowerCase())
       ?.scrollIntoView({ behavior: "smooth" })
+    props.changeActive(props.text)
   }
 
   return (

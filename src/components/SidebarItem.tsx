752c1ca23575dc36646a6670397349c52fccc054
GrzegorzJaskowski <grzegorz.jaskowski81@gmail.com>
2023-04-02 02:32:25 +0200

    fix: delete redundant code

--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -1,7 +1,5 @@
 import styles from "../styles/sidebarItem.module.css"
 
-function scrollToItem() {}
-
 export const SidebarItem = (props: { text: string }) => {
   function handleClick() {
     document
@@ -9,13 +7,7 @@ export const SidebarItem = (props: { text: string }) => {
       ?.scrollIntoView({ behavior: "smooth" })
   }
 
-  var id = "#"
-  id += props.text.toLowerCase()
   return (
-    // <a href={id} className={styles.sidebarItem}>
-    //   <p className={styles.header}>{props.text}</p>
-    // </a>
-
     <div onClick={handleClick} className={styles.sidebarItem}>
       <p className={styles.header}>{props.text}</p>
     </div>
